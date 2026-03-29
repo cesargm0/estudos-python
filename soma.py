8757672816ec0a49196e9dd347e7e20d64b30a14
Paulo César <paulocgm01@gmail.com>
2026-03-29 02:14:30 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,7 @@
+# crie um programa que mostre dois números e apresenta a soma entre eles
+x = float(input('Coloque o primeiro número: '))
+y = float(input('Coloque o segundo número: '))
+s = x + y
+print('A soma entre {} e {} é: {}' .format(x, y, s))
+
+# quis usar o format por ser mais organizado que usar aspas e virgulas para separar as variáveis
