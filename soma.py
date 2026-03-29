566f946788236b2175d333d4bf660ddb2b2970dd
Paulo César <paulocgm01@gmail.com>
2026-03-29 02:13:06 -0300

    Delete soma.py

--- soma.py
+++
@@ -1,7 +0,0 @@
-# crie um programa que mostre dois números e apresenta a soma entre eles
-x = float(input('Coloque o primeiro número: '))
-y = float(input('Coloque o segundo número: '))
-s = x + y
-print('A soma entre {} e {} é: {}' .format(x, y, s))
-
-# quis usar o format por ser mais organizado que usar aspas e virgulas para separar as variáveis
